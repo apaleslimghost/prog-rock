a597f7f7e28e31738e52938306fc8f46b309ab00
Matt Brennan <mattyb1000@gmail.com>
2016-03-04 13:19:35 +0000

    strip ansi, constantly pulse

--- index.js
+++ index.js
@@ -2,14 +2,18 @@
 
 var Gauge = require('gauge');
 var split = require('split');
+var strip = require('strip-ansi');
 var lines = parseInt(process.argv[2]);
 
 var gauge = new Gauge();
 gauge.setTheme(require('@quarterto/fancy-gauge'));
 
+var t = setInterval(() => gauge.pulse(), 100);
+
 var count = 0;
+process.stdin.on('end', () => clearInterval(t));
 process.stdin.pipe(split()).on('data', line => {
 	gauge.pulse();
-	gauge.show(line, count++ / lines);
+	gauge.show(strip(line), count++ / lines);
 });
 process.stdin.resume();
